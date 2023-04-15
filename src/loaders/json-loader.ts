d42313a9e62b847987433ae5a2b86fc854bc5cf7
Stephen Niedzielski <stephen@niedzielski.com>
2023-04-14 19:27:09 -0600

    Delete JSON loader

--- src/loaders/json-loader.ts
+++
@@ -1,8 +0,0 @@
-import { JSONValue } from '@/ooz'
-
-export async function loadJSON(source: string): Promise<JSONValue> {
-  const response = await fetch(source, {
-    headers: { 'Content-Type': 'application/json' },
-  })
-  return response.json()
-}
