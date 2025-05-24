c4354f4893b11d7c1d5bb04ed6705fbe9ff2c03b
Stephen Niedzielski <stephen@oidoid.com>
2025-05-24 13:57:56 -0600

    add 2D utils

--- src/types/geo.test.ts
+++ src/types/geo.test.ts
@@ -1,5 +1,16 @@
 import { assertEquals } from '@std/assert'
-import { xyAddTo, xyDiv } from './geo.ts'
+import {
+  xyAdd,
+  xyAddTo,
+  xyDistance,
+  xyDiv,
+  xyMagnitude,
+  xyStr,
+  xySub
+} from './geo.ts'
+
+Deno.test('xyAdd()', () =>
+  assertEquals(xyAdd({x: 1, y: 2}, {x: 3, y: 4}), {x: 4, y: 6}))
 
 Deno.test('xyAddTo()', () => {
   const xy = {x: 1, y: 2}
@@ -7,7 +18,15 @@ Deno.test('xyAddTo()', () => {
   assertEquals(xy, {x: 4, y: 6})
 })
 
-Deno.test('xyDiv()', () => {
-  const xy = xyDiv({x: 1, y: 2}, {x: 3, y: 4})
-  assertEquals(xy, {x: 1 / 3, y: 2 / 4})
-})
+Deno.test('xyDistance()', () =>
+  assertEquals(xyDistance({x: 1, y: 2}, {x: 6, y: 14}), 13))
+
+Deno.test('xyDiv()', () =>
+  assertEquals(xyDiv({x: 1, y: 2}, {x: 3, y: 4}), {x: 1 / 3, y: 2 / 4}))
+
+Deno.test('xyMagnitude()', () => assertEquals(xyMagnitude({x: 3, y: 4}), 5))
+
+Deno.test('xyStr()', () => assertEquals(xyStr({x: 1, y: 2}), '(1, 2)'))
+
+Deno.test('xySub()', () =>
+  assertEquals(xySub({x: 1, y: 2}, {x: 3, y: 4}), {x: -2, y: -2}))
