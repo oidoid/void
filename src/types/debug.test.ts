2a58df642249ed261c8670c49fd1df174ac0a5a9
Stephen Niedzielski <stephen@oidoid.com>
2025-08-09 21:17:16 -0600

    proxy debug

--- src/types/debug.test.ts
+++ src/types/debug.test.ts
@@ -3,43 +3,46 @@ import {test} from 'node:test'
 import {Debug} from './debug.ts'
 
 test('one param', () => {
-  const location = {href: 'https://oidoid.com/?debug=render'}
-  assert.deepEqual(Debug(location), {render: 'true'})
+  const url = 'https://oidoid.com/?debug=render'
+  assert.deepEqual(Debug(url), {render: 'true'})
 })
 
 test('debug and params are case-insensitive', () => {
-  const location = {href: 'https://oidoid.com/?Debug=Render=foo'}
-  assert.deepEqual(Debug(location), {render: 'foo'})
+  const url = 'https://oidoid.com/?Debug=Render=foo'
+  assert.deepEqual(Debug(url), {render: 'foo'})
 })
 
 test('multiple vals', () => {
-  const location = {href: 'https://oidoid.com/?debug=cam,input=bar,render=foo'}
-  assert.deepEqual(Debug(location), {cam: 'true', input: 'bar', render: 'foo'})
+  const url = 'https://oidoid.com/?debug=cam,input=bar,render=Foo'
+  assert.deepEqual(Debug(url), {cam: 'true', input: 'bar', render: 'Foo'})
 })
 
 test('multiple vals and params', () => {
-  const location = {
-    href: 'https://oidoid.com/?abc=1&debug=cam,input=foo,unknown0=bar,unknown1,render=baz,&def'
-  }
-  assert.deepEqual(Debug(location), {
+  const url =
+    'https://oidoid.com/?abc=1&debug=cam,input=foo,unknown0=Bar,Unknown1,render=baz,&def'
+
+  const debug = Debug(url)
+  assert.deepEqual(debug, {
     cam: 'true',
     input: 'foo',
-    unknown0: 'bar',
+    unknown0: 'Bar',
     unknown1: 'true',
     render: 'baz'
   })
+  assert.equal((debug as unknown as {Unknown1: string}).Unknown1, 'true')
+  assert.equal((debug as unknown as {unknown2: undefined}).unknown2, undefined)
 })
 
 test('no vals', () => {
-  const location = {href: 'https://oidoid.com/?debug'}
-  assert.deepEqual(Debug(location), {
-    cam: 'true',
-    input: 'true',
-    render: 'true'
-  })
+  const url = 'https://oidoid.com/?debug'
+  const debug = Debug(url)
+  assert.equal(debug?.cam, 'true')
+  assert.equal(debug?.input, 'true')
+  assert.equal(debug?.render, 'true')
+  assert.equal((debug as {unknown: string}).unknown, 'true')
 })
 
 test('no params', () => {
-  const location = {href: 'https://oidoid.com/'}
-  assert.deepEqual(Debug(location), undefined)
+  const url = 'https://oidoid.com/'
+  assert.deepEqual(Debug(url), undefined)
 })
--- src/types/debug.ts
+++ src/types/debug.ts
@@ -1,38 +1,43 @@
 /**
- * values are nonempty strings. each value defaults to `'true'` when key is
- * present. all values default to `'true'` when no `debug` param is set without
- * CSV.
+ * proxy for debug CSV query param with case-insensitive keys. when a key
+ * exists, the value is as specified or `'true'` if no value. all values default
+ * to `'true'` when the `debug` param is set without a CSV (eg,
+ * `localhost:1234?debug`). extend interface for additional types. doesn't
+ * handle multiple values like `localhost:1234?debug=a=1,b=2&debug=b=3`.
  */
-export type Debug =
-  | ({[K in 'cam' | 'input' | 'render']?: string} & {
-      [k: string]: string
-    })
-  | undefined
+export interface Debug {
+  cam?: string
+  input?: string
+  mem?: string
+  render?: string
+}
 
-export const debug: Readonly<Debug> = Debug(globalThis.location)
+export const debug: Readonly<Debug> | undefined = Debug(
+  globalThis.location?.href
+)
 
 /** @internal */
-export function Debug(location: {readonly href: string} | undefined): Debug {
-  if (!location) return
-  const csv = new URL(location.href.toLowerCase()).searchParams.get('debug')
+export function Debug(url: string | undefined): Debug | undefined {
+  if (!url) return
+  const csv = [...new URL(url).searchParams].find(
+    ([k]) => k.toLowerCase() === 'debug'
+  )?.[1]
   if (csv == null) return
   const map = Object.fromEntries(
     csv
-      .split(',')
-      .filter(Boolean)
-      .map(kv => kv.split('='))
+      .split(',') // split KV pairs.
+      .filter(Boolean) // drop empties.
+      .map(kv => kv.split('=')) // split each pair.
   )
-  const debug: {[k: string]: string} = {}
-  const keyset: {[_ in keyof Debug]-?: undefined} = {
-    cam: undefined,
-    input: undefined,
-    render: undefined
-  }
-  for (const k in keyset) {
-    const lowerK = k.toLowerCase()
-    if (!csv || lowerK in map) debug[k] = map[lowerK] || 'true'
-    delete map[lowerK]
-  }
-  for (const k in map) debug[k] = map[k] || 'true'
-  return debug
+
+  const target: {[k: PropertyKey]: string | undefined} = {}
+  for (const k in map) target[k.toLowerCase()] = map[k] || 'true'
+
+  return new Proxy<{[k: string]: string | undefined}>(target, {
+    get(target, k): string | undefined {
+      if (typeof k !== 'string') return target[k as unknown as string]
+      k = k.toLowerCase()
+      return (k in target && !target[k]) || !csv ? 'true' : target[k]
+    }
+  })
 }
