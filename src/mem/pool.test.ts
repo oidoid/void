ace79b59a92e0ecff08d84cd8215640b8694e5f5
Stephen Niedzielski <stephen@oidoid.com>
2025-08-17 09:51:42 -0600

    drop pool handles

--- src/mem/pool.test.ts
+++ src/mem/pool.test.ts
@@ -6,20 +6,20 @@ test('alloc()', async ctx => {
   const pool = new Pool({alloc: () => ({i: 0}), allocBytes: 5, pageBlocks: 2})
 
   ctx.test('has one page init', () => {
-    assert.equal(pool.toDebugString(), '000000000000000000 000000000000000000')
+    assert.equal(pool.toDebugString(), '0000000000 0000000000')
   })
 
   ctx.test('fills one page', () => {
     pool.alloc()
     pool.alloc()
-    assert.equal(pool.toDebugString(), '000000000000000000 000000000001000000')
+    assert.equal(pool.toDebugString(), '0000000000 0000000000')
   })
 
   ctx.test('grows to a second page', () => {
     pool.alloc()
     assert.equal(
       pool.toDebugString(),
-      '000000000000000000 000000000001000000 000000000002000000 000000000000000000'
+      '0000000000 0000000000 0000000000 0000000000'
     )
   })
 })
@@ -57,33 +57,24 @@ test('free()', async ctx => {
     assert.throws(() => pool.free({i: 0}))
   })
 
-  ctx.test("doesn't shrink", () => {
+  ctx.test('shrinks', () => {
     blocks.push(pool.alloc())
-    assert.equal(pool.toDebugString(), '000000000000000000 000000000000000000')
+    assert.equal(pool.toDebugString(), '0000000000 0000000000')
     blocks.push(pool.alloc())
     blocks.push(pool.alloc())
     assert.equal(
       pool.toDebugString(),
-      '000000000000000000 000000000001000000 000000000002000000 000000000000000000'
+      '0000000000 0000000000 0000000000 0000000000'
     )
     pool.free(blocks.shift()!)
-    assert.equal(
-      pool.toDebugString(),
-      '000000000002000000 000000000001000000 000000000002000000 000000000000000000'
-    )
+    assert.equal(pool.toDebugString(), '0000000000 0000000000')
     pool.free(blocks.shift()!)
-    assert.equal(
-      pool.toDebugString(),
-      '000000000002000000 000000000001000000 000000000002000000 000000000000000000'
-    )
+    assert.equal(pool.toDebugString(), '0000000000 0000000000')
   })
 
-  ctx.test('never shrinks', () => {
+  ctx.test('never shrinks below one page', () => {
     while (blocks.length) pool.free(blocks.shift()!)
-    assert.equal(
-      pool.toDebugString(),
-      '000000000002000000 000000000001000000 000000000002000000 000000000000000000'
-    )
+    assert.equal(pool.toDebugString(), '0000000000 0000000000')
   })
 })
 
@@ -103,28 +94,11 @@ test('size', async ctx => {
   })
 })
 
-test('stride', async ctx => {
-  ctx.test('is at least handle sized', () =>
-    assert.equal(
-      new Pool({alloc: () => ({i: 0}), allocBytes: 0, pageBlocks: 2}).stride,
-      4
-    )
-  )
-
-  ctx.test('is alloc + handle', () =>
-    assert.equal(
-      new Pool({alloc: () => ({i: 0}), allocBytes: 1, pageBlocks: 2}).stride,
-      5
-    )
-  )
-})
-
 test('alloc to capacity then free middle and realloc', () => {
   const pool = new Pool({
     alloc: () => ({
       i: 0,
       get(): number {
-        console.log(this.i)
         return pool.view.getUint8(this.i)
       },
       set(val: number): void {
@@ -135,7 +109,7 @@ test('alloc to capacity then free middle and realloc', () => {
     pageBlocks: 2
   })
 
-  assert.equal(pool.toDebugString(), '0000000000 0000000000')
+  assert.equal(pool.toDebugString(), '00 00')
 
   const blocks = []
   for (let i = 0; i < 6; i++) {
@@ -148,84 +122,59 @@ test('alloc to capacity then free middle and realloc', () => {
     blocks.map(block => block.get()),
     [0, 1, 2, 3, 4, 5]
   )
-  assert.equal(
-    pool.toDebugString(),
-    '0000000000 0101000000 0202000000 0303000000 0404000000 0505000000'
-  )
+  assert.equal(pool.toDebugString(), '00 01 02 03 04 05')
 
   pool.free(blocks[2]!)
-  assert.equal(blocks[2]!.i, 6)
-  assert.equal(blocks[5]!.i, 10)
+  assert.equal(blocks[5]!.i, 2)
   blocks.splice(2, 1)
-  console.log('ok')
   assert.deepEqual(
     blocks.map(block => block.get()),
     [0, 1, 3, 4, 5]
   )
-  assert.equal(
-    pool.toDebugString(),
-    '0000000000 0101000000 0505000000 0303000000 0404000000 0505000000'
-  )
+  assert.equal(pool.toDebugString(), '00 01 05 03 04 05')
 
   pool.free(blocks[2]!)
-  assert.equal(blocks[2]!.i, 2)
-  assert.equal(blocks[3]!.i, 15)
+  assert.equal(blocks[3]!.i, 3)
   blocks.splice(2, 1)
   assert.deepEqual(
     blocks.map(block => block.get()),
     [0, 1, 4, 5]
   )
-  assert.equal(
-    pool.toDebugString(),
-    '0000000000 0101000000 0505000000 0404000000 0404000000 0505000000'
-  )
+  assert.equal(pool.toDebugString(), '00 01 05 04')
 
   pool.free(blocks[2]!)
-  assert.equal(blocks[2]!.i, 3)
-  assert.equal(blocks[3]!.i, 10)
+  assert.equal(blocks[3]!.i, 2)
   blocks.splice(2, 1)
   assert.deepEqual(
     blocks.map(block => block.get()),
     [0, 1, 5]
   )
-  assert.equal(
-    pool.toDebugString(),
-    '0000000000 0101000000 0505000000 0404000000 0404000000 0505000000'
-  )
+  assert.equal(pool.toDebugString(), '00 01 05 04')
 
   blocks.push(pool.alloc())
-  assert.equal(blocks[3]!.i, 15)
+  assert.equal(blocks[3]!.i, 3)
   blocks[3]!.set(2)
   assert.deepEqual(
     blocks.map(block => block.get()),
     [0, 1, 5, 2]
   )
-  assert.equal(
-    pool.toDebugString(),
-    '0000000000 0101000000 0505000000 0204000000 0404000000 0505000000'
-  )
+  assert.equal(pool.toDebugString(), '00 01 05 02')
 
   blocks.push(pool.alloc())
-  assert.equal(blocks[4]!.i, 20)
+  assert.equal(blocks[4]!.i, 4)
   blocks[4]!.set(3)
   assert.deepEqual(
     blocks.map(block => block.get()),
     [0, 1, 5, 2, 3]
   )
-  assert.equal(
-    pool.toDebugString(),
-    '0000000000 0101000000 0505000000 0204000000 0303000000 0505000000'
-  )
+  assert.equal(pool.toDebugString(), '00 01 05 02 03 00')
 
   blocks.push(pool.alloc())
-  assert.equal(blocks[5]!.i, 25)
+  assert.equal(blocks[5]!.i, 5)
   blocks[5]!.set(4)
   assert.deepEqual(
     blocks.map(block => block.get()),
     [0, 1, 5, 2, 3, 4]
   )
-  assert.equal(
-    pool.toDebugString(),
-    '0000000000 0101000000 0505000000 0204000000 0303000000 0402000000'
-  )
+  assert.equal(pool.toDebugString(), '00 01 05 02 03 04')
 })
--- src/mem/pool.ts
+++ src/mem/pool.ts
@@ -10,95 +10,75 @@ export type PoolOpts<T extends Block> = {
 export type Block = {/** variable pool byte offset. */ i: number}
 
 /**
- * a contiguous growable array with handle objects.
+ * a contiguous resizable TypedArray associated with an array of objects.
  *
  * alternatives:
  * - operating on the array without classes would probably be faster but the DX
  *   sucks:
  *     this.#sprite = this.#pool.alloc()
  *     this.#pool.setX(this.#sprite, this.#pool.getX(this.#sprite) + 1)
- * - setting a discard flag on sprites instead of handles may be more efficient
- *   but requires copying all elements on every frame and is sprite specific.
  * - sprite classes owning all the data is nice DX but requires a copy on every
  *   frame.
  * - ArrayBuffer.resize() is incompatible with WebGL.
  */
 export class Pool<T extends Block> {
-  /** sized to one page initially but only grows. blocks fragment on free. */
+  /** the next free block is always at size. */
   readonly #blocks: T[]
-  /** the head of the free block linked list indexing into #blocks. */
-  #free: number = 0
-  readonly #opts: Readonly<PoolOpts<T>>
+  readonly #opts: Readonly<Required<PoolOpts<T>>>
   #size: number = 0
-  /** the next free block is always at size. resized to n pages. */
   #u8: Uint8Array<ArrayBuffer>
-  #view: DataView<ArrayBuffer>
+  #view!: DataView<ArrayBuffer>
 
   constructor(opts: Readonly<PoolOpts<T>>) {
-    this.#opts = opts
-    const initSize = (opts.minPages ?? 1) * opts.pageBlocks
-
-    const buffer = new ArrayBuffer(this.stride * initSize)
-    this.#u8 = new Uint8Array(buffer)
-    this.#view = new DataView(buffer)
-
-    this.#blocks = Array(initSize)
-    for (let i = 0; i < initSize; i++) {
-      this.#blocks[i] = opts.alloc(this)
-      this.#blocks[i]!.i = i + 1 // next handle.
-    }
+    if (opts.allocBytes <= 0) throw Error('byte allocation must be > 0')
+    if (opts.pageBlocks <= 0) throw Error('page blocks must be > 0')
+    this.#opts = {...opts, minPages: opts.minPages ?? 1}
+    const initSize = this.#opts.minPages * opts.pageBlocks
+    this.#blocks = new Array(initSize)
+    this.#u8 = new Uint8Array()
+    this.#resize(initSize, 'Init')
   }
 
   alloc(): T {
-    if (this.#size >= this.#currentCapacity) {
-      const capacity = this.#currentCapacity + this.#opts.pageBlocks
-      if (debug?.mem) console.debug(`[mem] growing pool to ${capacity}`)
-
-      for (let i = this.#currentCapacity; i < capacity; i++) {
-        this.#blocks[i] = this.#opts.alloc(this)
-        this.#blocks[i]!.i = i + 1 // next handle.
-      }
-
-      const buffer = new ArrayBuffer(this.stride * capacity)
-      const u8 = new Uint8Array(buffer)
-      u8.set(this.#u8)
-      this.#u8 = u8
-      this.#view = new DataView(buffer)
-    }
-    const handle = this.#free
-    const block = this.#blocks[handle]!
-    this.#free = block.i
-    block.i = this.#size * this.stride
-    this.#setHandle(this.#size, handle)
+    if (this.#size >= this.#currentCapacity)
+      this.#resize(this.#currentCapacity + this.#opts.pageBlocks, 'Call')
+    const block = this.#blocks[this.#size]!
+    block.i = this.#size * this.#opts.allocBytes
     this.#size++
     return block
   }
 
   clear(): void {
-    for (let i = 0; i < this.#blocks.length; i++) this.#blocks[i]!.i = i + 1
-    this.#free = 0
     this.#size = 0
+    const minCapacity = this.#opts.minPages * this.#opts.pageBlocks
+    if (this.#currentCapacity > minCapacity) this.#resize(minCapacity, 'Call')
   }
 
   free(block: T): void {
     if (!this.#size) throw Error('pool underflow')
 
-    const handle = this.#getHandle(block.i / this.stride)
     this.#size--
-    const start = this.#size * this.stride
-    this.#u8.copyWithin(block.i, start, start + this.stride)
-    this.#blocks[this.#getHandle(this.#size)]!.i = block.i
+    const start = this.#size * this.#opts.allocBytes
+    this.#u8.copyWithin(block.i, start, start + this.#opts.allocBytes)
+    this.#blocks[this.#size]!.i = block.i
+    ;[this.#blocks[block.i], this.#blocks[this.#size]] = [
+      this.#blocks[this.#size]!,
+      this.#blocks[block.i]!
+    ]
 
-    block.i = this.#free
-    this.#free = handle
+    const free = Math.floor(
+      (this.#currentCapacity - this.#size) / this.#opts.pageBlocks
+    )
+    if (
+      free &&
+      this.#currentCapacity / this.#opts.pageBlocks - free >=
+        this.#opts.minPages
+    )
+      this.#resize(this.#currentCapacity - free * this.#opts.pageBlocks, 'Call')
   }
 
   get size(): number {
     return this.#size
-  }
-
-  get stride(): number {
-    return this.#opts.allocBytes + 4
   }
 
   get view(): DataView<ArrayBuffer> {
@@ -108,21 +88,30 @@ export class Pool<T extends Block> {
   toDebugString(): string {
     let str = ''
     for (let i = 0; i < this.#u8.buffer.byteLength; i++) {
-      if (i && !(i % this.stride)) str += ' '
+      if (i && !(i % this.#opts.allocBytes)) str += ' '
       str += this.#u8[i]!.toString(16).padStart(2, '0')
     }
     return str
   }
 
   get #currentCapacity(): number {
-    return this.#u8.buffer.byteLength / this.stride
+    return this.#u8.buffer.byteLength / this.#opts.allocBytes
   }
 
-  #getHandle(i: number): number {
-    return this.view.getUint32(i * this.stride + this.#opts.allocBytes, true)
-  }
+  #resize(capacity: number, origin: 'Init' | 'Call'): void {
+    if (origin === 'Init' && debug?.mem)
+      console.debug(
+        `[mem] ${capacity < this.#currentCapacity ? 'shrinking' : 'growing'} pool to ${capacity}`
+      )
 
-  #setHandle(i: number, handle: number): void {
-    this.view.setUint32(i * this.stride + this.#opts.allocBytes, handle, true)
+    this.#blocks.length = capacity
+    for (let i = this.#currentCapacity; i < capacity; i++)
+      this.#blocks[i] = this.#opts.alloc(this)
+
+    const buffer = new ArrayBuffer(capacity * this.#opts.allocBytes)
+    const u8 = new Uint8Array(buffer)
+    u8.set(this.#u8.subarray(0, capacity * this.#opts.allocBytes))
+    this.#u8 = u8
+    this.#view = new DataView(buffer)
   }
 }
