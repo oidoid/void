7f142f5401e5a807a9668efb372b8b4cf8f40268
Stephen Niedzielski <stephen@oidoid.com>
2025-09-04 21:57:14 -0600

    invalidate input when a gamepad is connected

--- src/input/gamepad.test.ts
+++ src/input/gamepad.test.ts
@@ -4,22 +4,32 @@ import {Gamepad} from './gamepad.ts'
 
 test('Gamepad', async ctx => {
   globalThis.isSecureContext = true
-  const gamepad = new Gamepad()
+  const target = new EventTarget()
+  using gamepad = new Gamepad(target).register('add')
   gamepad.bitByButton[0] = 1
 
-  ctx.test('init', () => assert.equal(gamepad.bits, 0))
+  ctx.test('init', () => {
+    Object.assign(globalThis.navigator, {getGamepads: () => [null]})
+    assert.equal(gamepad.bits, 0)
+    assert.equal(gamepad.invalid, false)
+  })
 
   ctx.test('no pads', () => {
     Object.assign(globalThis.navigator, {getGamepads: () => []})
     gamepad.update()
     assert.equal(gamepad.bits, 0)
+    assert.equal(gamepad.invalid, false)
+    gamepad.postupdate()
   })
 
   ctx.test('one pad', () => {
     Object.assign(globalThis.navigator, {
       getGamepads: () => [{axes: [], buttons: [{pressed: true}]}]
     })
+    assert.equal(gamepad.invalid, true)
     gamepad.update()
     assert.equal(gamepad.bits, 1)
+    assert.equal(gamepad.invalid, true)
+    gamepad.postupdate()
   })
 })
