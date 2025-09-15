e376c95af6300e1e382dde2c9ad08b6fb8d71697
Stephen Niedzielski <stephen@oidoid.com>
2025-09-14 21:07:41 -0600

    workaround mobile precision issues

--- src/graphics/sprite-frag.glsl.ts
+++ src/graphics/sprite-frag.glsl.ts
@@ -2,21 +2,25 @@ export const spriteFragGLSL: string = `#version 300 es
 #pragma debug(${debug?.render ? 'on' : 'off'})   
 #pragma optimize(${debug?.render ? 'off' : 'on'})
 
-uniform mediump sampler2D uTex;
-uniform mediump uvec2 uTexWH;
+precision highp int;
+precision highp float;
+precision highp usampler2D;
 
-flat in lowp uint vStretch;
-flat in mediump ivec4 vTexXYWH;
-flat in lowp int vZ;
-in highp vec2 vDstWH;
-flat in highp ivec2 vDstWHFixed;
+uniform sampler2D uTex;
+uniform uvec2 uTexWH;
 
-out highp vec4 oRGBA;
+flat in uint vStretch;
+flat in ivec4 vTexXYWH;
+flat in int vZ;
+in vec2 vDstWH;
+flat in ivec2 vDstWHFixed;
+
+out vec4 oRGBA;
 
 void main() {
-  if (vZ == ${Layer.Hidden}) discard;
+  if (vZ == ${Layer.Hidden} || vDstWHFixed.x == 0 || vDstWHFixed.y == 0) discard;
 
-  highp vec2 px = vec2(vTexXYWH.xy) + (vStretch == 1u ? (vDstWH * vec2(vTexXYWH.zw) / vec2(vDstWHFixed)) : mod(vDstWH, vec2(vTexXYWH.zw)));
+  vec2 px = vec2(vTexXYWH.xy) + (vStretch == 1u ? (vDstWH * vec2(vTexXYWH.zw) / vec2(vDstWHFixed)) : mod(vDstWH, vec2(vTexXYWH.zw)));
 
   oRGBA = texture(uTex, px / vec2(uTexWH));
   if(oRGBA.a < .001) discard;
--- src/graphics/sprite-vert.glsl.ts
+++ src/graphics/sprite-vert.glsl.ts
@@ -3,53 +3,56 @@ export const spriteVertGLSL: string = `#version 300 es
 #pragma optimize(${debug?.render ? 'off' : 'on'})
 
 // https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_best_practices#essl300_minimum_requirements_webgl_2
+precision highp int;
+precision highp float;
+precision highp usampler2D;
 
-uniform highp ivec4 uCam;
-uniform lowp usampler2D uCels;
-uniform highp float uAge;
+uniform ivec4 uCam;
+uniform usampler2D uCels;
+uniform float uAge;
 
-layout (location=0) in lowp ivec2 iUV;
-layout (location=1) in highp uint iy8_x24;
-layout (location=2) in highp uint iw8_sxyz_llll_y16;
-layout (location=3) in highp uint ii11_c5_h12_w4;
+layout (location=0) in ivec2 iUV;
+layout (location=1) in uint iy8_x24;
+layout (location=2) in uint iw8_sxyz_llll_y16;
+layout (location=3) in uint ii11_c5_h12_w4;
 
-const highp int maxY = 0x20000;
-const lowp int layers = 16;
-const highp int maxDepth = maxY * layers;
+const int maxY = 0x20000;
+const int layers = 16;
+const int maxDepth = maxY * layers;
 
-flat out lowp uint vStretch;
-flat out mediump ivec4 vTexXYWH;
-flat out lowp int vZ;
-out highp vec2 vDstWH;
-flat out highp ivec2 vDstWHFixed;
+flat out uint vStretch;
+flat out ivec4 vTexXYWH;
+flat out int vZ;
+out vec2 vDstWH;
+flat out ivec2 vDstWHFixed;
 
 void main() {
-  highp int x = (int((iy8_x24 & 0xffffffu) << 8) >> 8) / 64;
-  highp int y = (int(iy8_x24 >> 24) | (int(iw8_sxyz_llll_y16 & 0xffffu) << 16 >> 8)) / 64;
-  lowp int z = int((iw8_sxyz_llll_y16 >> 16) & 0xfu);
+  int x = (int((iy8_x24 & 0xffffffu) << 8) >> 8) / 64;
+  int y = (int(iy8_x24 >> 24) | (int(iw8_sxyz_llll_y16 & 0xffffu) << 16 >> 8)) / 64;
+  int z = int((iw8_sxyz_llll_y16 >> 16) & 0xfu);
   bool zend = bool(iw8_sxyz_llll_y16 & 0x100000u);
   bool flipY = bool(iw8_sxyz_llll_y16 & 0x200000u);
   bool flipX = bool(iw8_sxyz_llll_y16 & 0x400000u);
   bool stretch = bool(iw8_sxyz_llll_y16 & 0x800000u);
-  mediump int id = int((ii11_c5_h12_w4 >> 17) & 0x7ff0u); // ignore cel.
-  lowp int cel = int((ii11_c5_h12_w4 >> 16) & 0xfu); // ignore the MSB.
-  mediump int w = int(((ii11_c5_h12_w4 & 0xfu) << 8) | (iw8_sxyz_llll_y16 >> 24));
-  mediump int h = int((ii11_c5_h12_w4 >> 4) & 0xfffu);
+  int id = int((ii11_c5_h12_w4 >> 17) & 0x7ff0u); // ignore cel.
+  int cel = int((ii11_c5_h12_w4 >> 16) & 0xfu); // ignore the MSB.
+  int w = int(((ii11_c5_h12_w4 & 0xfu) << 8) | (iw8_sxyz_llll_y16 >> 24));
+  int h = int((ii11_c5_h12_w4 >> 4) & 0xfffu);
 
   // https://www.patternsgameprog.com/opengl-2d-facade-25-get-the-z-of-a-pixel
-  highp float depth = float((z + 1) * maxY - (y + (zend ? 0 : h))) / float(maxDepth);
+  float depth = float((z + 1) * maxY - (y + (zend ? 0 : h))) / float(maxDepth);
 
-  highp ivec2 targetWH = iUV * ivec2(w, h);
+  ivec2 targetWH = iUV * ivec2(w, h);
 
-  highp vec2 end = vec2(x + targetWH.x, y + targetWH.y);
+  vec2 end = vec2(x + targetWH.x, y + targetWH.y);
   // UI layers are always given in screen coordinates.
   // to-do: how to handle non-int mode?
-  highp vec2 camXY = z <= ${Layer.UIBottom} ? vec2(0, 0) : vec2(uCam.xy);
-  highp vec2 clip = ((-2. * camXY  + 2. * end) / vec2(uCam.zw) - 1.) * vec2(1, -1);
+  vec2 camXY = z <= ${Layer.UIBottom} ? vec2(0, 0) : vec2(uCam.xy);
+  vec2 clip = ((-2. * camXY  + 2. * end) / vec2(uCam.zw) - 1.) * vec2(1, -1);
   gl_Position = vec4(clip, depth, 1);
 
-  lowp int frame = (((int(uAge / ${celMillis}) & 0x1f) - cel) + ${animCels}) & 0xf;
-  mediump uvec4 texXYWH = texelFetch(uCels, ivec2(0, id + frame), 0);
+  int frame = (((int(uAge / ${celMillis}) & 0x1f) - cel) + ${animCels}) & 0xf;
+  uvec4 texXYWH = texelFetch(uCels, ivec2(0, id + frame), 0);
   vTexXYWH = ivec4(texXYWH);
   vZ = z;
 
