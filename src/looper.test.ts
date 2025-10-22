bab755a08715272f957c3e55ee5d02881b1fec4e
Stephen Niedzielski <stephen@oidoid.com>
2025-10-22 16:25:50 -0600

    favor looper test steps

--- src/looper.test.ts
+++ src/looper.test.ts
@@ -5,9 +5,10 @@ import {Looper} from './looper.ts'
 import {TestEvent} from './test/test-event.ts'
 import type {Millis} from './types/time.ts'
 
-let oldNow: () => Millis
 const doc = Object.assign(new EventTarget(), {hidden: false})
+let oldNow: () => Millis
 let onFrame: (() => void) | undefined
+
 beforeEach(() => {
   oldNow = performance.now
   globalThis.document = doc as Document
@@ -23,49 +24,52 @@ afterEach(() => {
   delete (globalThis as {[_: string]: unknown}).cancelAnimationFrame
   delete (globalThis as {[_: string]: unknown}).requestAnimationFrame
 })
-using framer = new Looper()
-let frame = 0
-framer.onFrame = () => ++frame
 
-test('init', () => assert.equal(frame, 0))
+test('Looper', ctx => {
+  using framer = new Looper()
+  let frame = 0
+  framer.onFrame = () => ++frame
 
-test('register', () => {
-  framer.register('add')
-  assert.equal(frame, 0)
-  assert.equal(framer.age, 0)
-})
+  ctx.test('init', () => assert.equal(frame, 0))
 
-test('onFrame', () => {
-  performance.now = () => 0
-  framer.requestFrame()
-  performance.now = () => 10 as Millis
-  onFrame!()
-  assert.equal(frame, 1)
-  assert.equal(framer.age, 10)
-  framer.requestFrame()
-  performance.now = () => 20 as Millis
-  onFrame!()
-  assert.equal(frame, 2)
-  assert.equal(framer.age, 20)
-  framer.requestFrame()
-  performance.now = () => 30 as Millis
-  onFrame!()
-  assert.equal(frame, 3)
-  assert.equal(framer.age, 30)
-})
+  ctx.test('register', () => {
+    framer.register('add')
+    assert.equal(frame, 0)
+    assert.equal(framer.age, 0)
+  })
 
-test('hidden', () => {
-  framer.requestFrame()
-  doc.hidden = true
-  doc.dispatchEvent(TestEvent('visibilitychange'))
-  assert.equal(onFrame, undefined)
-})
+  ctx.test('onFrame', () => {
+    performance.now = () => 0
+    framer.requestFrame()
+    performance.now = () => 10 as Millis
+    onFrame!()
+    assert.equal(frame, 1)
+    assert.equal(framer.age, 10)
+    framer.requestFrame()
+    performance.now = () => 20 as Millis
+    onFrame!()
+    assert.equal(frame, 2)
+    assert.equal(framer.age, 20)
+    framer.requestFrame()
+    performance.now = () => 30 as Millis
+    onFrame!()
+    assert.equal(frame, 3)
+    assert.equal(framer.age, 30)
+  })
 
-test('shown', () => {
-  doc.hidden = false
-  doc.dispatchEvent(TestEvent('visibilitychange'))
-  performance.now = () => 40 as Millis
-  onFrame!()
-  assert.equal(frame, 4)
-  assert.equal(framer.age, 40)
+  ctx.test('hidden', () => {
+    framer.requestFrame()
+    doc.hidden = true
+    doc.dispatchEvent(TestEvent('visibilitychange'))
+    assert.equal(onFrame, undefined)
+  })
+
+  ctx.test('shown', () => {
+    doc.hidden = false
+    doc.dispatchEvent(TestEvent('visibilitychange'))
+    performance.now = () => 40 as Millis
+    onFrame!()
+    assert.equal(frame, 4)
+    assert.equal(framer.age, 40)
+  })
 })
