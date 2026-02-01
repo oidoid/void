7830ddd42c8b53378bbe5c55a8c25161b498c85a
Stephen Niedzielski <stephen@oidoid.com>
2026-02-01 10:16:42 -0700

    drop headless dev container config; add Aseprite + cwebp dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,23 @@
 set -eu
 
 sudo apt update
+
+# cwebp.
+sudo apt install --no-install-recommends --yes webp
+
+# Aseprite.
+sudo apt install \
+  --no-install-recommends \
+  --yes \
+  libfontconfig1 \
+  libgl1 \
+  libx11-6 \
+  libxcb1 \
+  libxcursor1 \
+  libxext6 \
+  libxrandr2 \
+
+# `xvfb-run`.
 sudo apt install --no-install-recommends --yes xauth xvfb
 
 npm install
-npx playwright install --with-deps
