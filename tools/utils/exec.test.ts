45766c949e3ccd93ff44ecf3f6caba49f4f2d5d5
Stephen Niedzielski <stephen@oidoid.com>
2026-02-08 18:20:09 -0700

    move `exec()` stdin option to dottable

--- tools/utils/exec.test.ts
+++ tools/utils/exec.test.ts
@@ -8,15 +8,37 @@ test('exec runs command and returns stdout', async () => {
   assert(stdout, 'ok\n')
 })
 
-test('exec passes stdin option', async () => {
+test('exec supports stdin chaining', async () => {
   const script = 'process.stdin.on("data",c=>process.stdout.write(c))'
-  const stdout =
-    await exec`${process.execPath} --eval ${script} ${{stdin: 'hello'}}`
+  const stdout = await exec`${process.execPath} --eval ${script}`.stdin('hello')
   assert(stdout, 'hello')
 })
 
-test('exec throws on missing exe', async () =>
-  await assert.rejects(exec``, /exe missing/))
+test('exec then transforms output', async () => {
+  const stdout = await exec`echo hi`.then(val => val.toUpperCase())
+  assert(stdout, 'HI\n')
+})
+
+test('catch()', async () => {
+  const script = 'process.exit(1)'
+  const stdout = await exec`${process.execPath} --eval ${script}`.catch(
+    () => 'recovered'
+  )
+  assert(stdout, 'recovered')
+})
+
+test('finally()', async () => {
+  let called = false
+  const stdout = await exec`echo hi`.finally(() => {
+    called = true
+  })
+  assert(called, true)
+  assert(stdout, 'hi\n')
+})
+
+test('exec throws on missing exe', () => {
+  assert.throws(() => exec``, /exe missing/)
+})
 
 test('miscellaneous', async () => {
   assert(await exec`echo`, '\n')
@@ -29,7 +51,6 @@ test('miscellaneous', async () => {
   assert(await exec`echo a,b c.d e:f`, 'a,b c.d e:f\n')
   assert(await exec`echo foo/bar baz/qux`, 'foo/bar baz/qux\n')
   assert(await exec`echo foo\\bar`, 'foo\\bar\n')
-  assert(await exec`echo one\t\ttwo`, 'one two\n')
   assert(await exec`echo one   two`, 'one two\n')
   assert(await exec`   echo one`, 'one\n')
   assert(await exec`echo one   `, 'one\n')
--- tools/utils/exec.ts
+++ tools/utils/exec.ts
@@ -1,38 +1,62 @@
 import {execFile} from 'node:child_process'
 import util from 'node:util'
-import {isRecord} from '../../src/utils/obj-util.ts'
 
-export type ExecOpts = {stdin?: string}
+class ExecCommand implements PromiseLike<string> {
+  readonly #exe: string
+  readonly #args: readonly string[]
+  #stdin?: string
+
+  constructor(exe: string, args: readonly string[]) {
+    this.#exe = exe
+    this.#args = args
+  }
+
+  stdin(stdin: string): ExecCommand {
+    this.#stdin = stdin
+    return this
+  }
+
+  async run(): Promise<string> {
+    const promise = util.promisify(execFile)(this.#exe, this.#args, {})
+    if (promise.child.stdin && this.#stdin != null) {
+      promise.child.stdin.write(this.#stdin)
+      promise.child.stdin.end()
+    }
+    const {stdout, stderr} = await promise
+    process.stderr.write(stderr)
+    return stdout
+  }
+
+  // biome-ignore lint/suspicious/noThenProperty:;
+  then<Fulfil = string, Reject = never>(
+    onFulfilled?: (val: string) => Fulfil | PromiseLike<Fulfil>,
+    onRejected?: (reason: unknown) => Reject | PromiseLike<Reject>
+  ): Promise<Fulfil | Reject> {
+    return this.run().then(onFulfilled, onRejected)
+  }
+
+  catch<Result = never>(
+    onRejected?: (reason: unknown) => Result | PromiseLike<Result>
+  ): Promise<string | Result> {
+    return this.run().catch(onRejected)
+  }
+
+  finally(onFinished?: () => void): Promise<string> {
+    return this.run().finally(onFinished)
+  }
+}
 
 /**
  * ```ts
- * await exec`tee --append log.text ${{stdin: 'hello\n'}}`
+ * await exec`tee --append log.text`.stdin('hello\n')
  * ```
  */
-export async function exec(
+export function exec(
   raw: TemplateStringsArray,
   ...exprs: readonly unknown[]
-): Promise<string> {
-  const {exe, args, opts} = parse(raw, exprs)
-  const promise = util.promisify(execFile)(exe, args, {})
-  if (promise.child.stdin && opts?.stdin != null) {
-    promise.child.stdin.write(opts.stdin)
-    promise.child.stdin.end()
-  }
-  const {stdout, stderr} = await promise
-  process.stderr.write(stderr)
-  return stdout
-}
-
-function parse(
-  raw: TemplateStringsArray,
-  exprs: readonly unknown[]
-): {exe: string; args: string[]; opts: ExecOpts | undefined} {
-  const opts = isRecord(exprs.at(-1)) ? exprs.at(-1)! : undefined
-  const parts = opts ? exprs.slice(0, -1) : exprs
-  const [exe, ...args] = String.raw({raw}, ...parts)
-    .trim()
-    .split(/\s+/)
+): ExecCommand {
+  const str = String.raw({raw}, ...exprs)
+  const [exe, ...args] = str.trim().split(/\s+/)
   if (!exe) throw Error('exe missing')
-  return {exe, args, opts}
+  return new ExecCommand(exe, args)
 }
