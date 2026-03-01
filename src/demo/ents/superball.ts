7a8ce0a6d520b0f309873faca382985721facfac
Stephen Niedzielski <stephen@oidoid.com>
2026-03-01 09:27:26 -0700

    improve superball hit detection and perf

--- src/demo/ents/superball.ts
+++ src/demo/ents/superball.ts
@@ -6,32 +6,33 @@ export class SuperballHook implements V.Hook {
   readonly query = 'superball & sprite'
 
   update(ent: SuperballEnt, v: V.Void): void {
-    if (!v.level || !ent.sprite.hitbox) return
-
     ent.sprite.x += ent.superball.vx * v.tick.s
     ent.sprite.y += ent.superball.vy * v.tick.s
 
+    const {hitbox} = ent.sprite
+    if (!v.level || !hitbox) return
+
     const bounds = v.level
     const left = bounds.x
-    const right = bounds.x + bounds.w - ent.sprite.hitbox.w
+    const right = bounds.x + bounds.w - hitbox.w
     const top = bounds.y
-    const bottom = bounds.y + bounds.h - ent.sprite.hitbox.h
+    const bottom = bounds.y + bounds.h - hitbox.h
 
-    if (ent.sprite.x < left) {
+    if (hitbox.x < left) {
       ent.sprite.x = left
       ent.superball.vx = Math.abs(ent.superball.vx)
       ent.superball.vy += (v.random.num - 0.5) * 20
-    } else if (ent.sprite.x > right) {
+    } else if (hitbox.x > right) {
       ent.sprite.x = right
       ent.superball.vx = -Math.abs(ent.superball.vx)
       ent.superball.vy += (v.random.num - 0.5) * 20
     }
 
-    if (ent.sprite.y < top) {
+    if (hitbox.y < top) {
       ent.sprite.y = top
       ent.superball.vy = Math.abs(ent.superball.vy)
       ent.superball.vx += (v.random.num - 0.5) * 20
-    } else if (ent.sprite.y > bottom) {
+    } else if (hitbox.y > bottom) {
       ent.sprite.y = bottom
       ent.superball.vy = -Math.abs(ent.superball.vy)
       ent.superball.vx += (v.random.num - 0.5) * 20
