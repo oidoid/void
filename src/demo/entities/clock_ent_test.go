03a817f930a2f730b95929fdb455267ed25f4eab
Stephen Niedzielski <stephen@oidoid.com>
2026-08-02 09:27:02 -0600

    add some clock tests

--- src/demo/entities/clock_ent_test.go
+++ src/demo/entities/clock_ent_test.go
@@ -34,17 +34,23 @@ func TestTimeString(t *testing.T) {
 
 func TestMillisToNextMin(t *testing.T) {
 	for _, test := range []struct {
+		name   string
 		millis uint64
 		want   uint64
 	}{
-		{0, 60_000},
-		{1, 59_999},
-		{59_999, 1},
-		{60_000, 60_000},
-		{123_456_789, 23_211},
+		{"zero", 0, 60_000},
+		{"one", 1, 59_999},
+		{"two", 2, 59_998},
+		{"one second", 1_000, 59_000},
+		{"one second one", 1_001, 58_999},
+		{"last", 59_999, 1},
 	} {
-		if got := millisToNextMin(test.millis); got != test.want {
-			t.Errorf("millisToNextMin(%d) = %d, want %d", test.millis, got, test.want)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			if got := millisToNextMin(test.millis); got != test.want {
+				t.Errorf("millisToNextMin(%d) = %d, want %d",
+					test.millis, got, test.want,
+				)
+			}
+		})
 	}
 }
